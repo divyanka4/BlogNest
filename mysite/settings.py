eff3fbc1f3dfa577e5148a81afbd1cd714870c52
divyanka4 <paldivyankaa7@gmmail.com>
2025-08-18 15:41:52 +0530

    Update settings.py: fix ALLOWED_HOSTS and add middleware

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'blognest-vhol.onrender.com'
+]
 
 
 # Application definition

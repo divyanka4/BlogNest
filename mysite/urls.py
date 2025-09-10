dc0186bbc0abd3a37e6b789a99881164c661e850
divyanka4 <paldivyankaa7@gmmail.com>
2025-09-10 13:06:31 +0530

    Updated urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,7 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('blog.urls')), 
     path('accounts/', include('allauth.urls')), # This includes all blog URLs including authentication
-    path('admin/', admin.site.urls),
-    path("ckeditor5/", include('django_ckeditor_5.urls')),   
+    path('admin/', admin.site.urls), 
     path("ckeditor5/", include("django_ckeditor_5.urls")), # upload endpoint
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
